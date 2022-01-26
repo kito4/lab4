9aed5a640d8158e0fc7109eb61687b9a1445b82b
kito4 <53493546+kito4@users.noreply.github.com>
2022-01-26 18:17:25 +0300

    Add files via upload

--- src/classes/Background.java
+++ src/classes/Background.java
@@ -6,7 +6,21 @@ public class Background extends Location {
     public Background(String name) {
         super(name);
     }
+    public static class Weather{
+        String status;
 
+        public Weather(String status) {
+            this.status = status;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+    }
     @Override
     public String toString() {
         return "Background{" +
